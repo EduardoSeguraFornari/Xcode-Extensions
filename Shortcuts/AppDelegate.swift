98824e350777c6d2d73c5fb797d4f9a980dbb9a4
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-11-04 21:55:30 -0300

    Initial Commit

---
+++ Shortcuts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shortcuts
+//
+//  Created by Eduardo Fornari on 04/11/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shortcuts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shortcuts
+//
+//  Created by Eduardo Fornari on 04/11/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
